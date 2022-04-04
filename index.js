f3e40956ce50d7931a1e8f362c31457f758407b7
youbaidjis <youba.etud@gmail.com>
2022-04-04 01:13:59 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+
+function findMatching(data, nameIn) {
+    return data.filter((nameSearched) => nameSearched.toLowerCase() === nameIn.toLowerCase())
+  }
+
+function fuzzyMatch(dataIn, lettersIn){
+    return dataIn.filter(function (letterSearched) {
+            return letterSearched.substring(0, 2) == lettersIn;
+        })
+}
+function matchName(arr, stringIn){
+    return arr.filter(function (driver){ return driver.name.toLowerCase() == stringIn.toLowerCase()
+     })
+}
